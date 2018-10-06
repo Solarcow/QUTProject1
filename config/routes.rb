9343a444bee72a5c855d99062a0317f8856ac0e3
Solarcow <Alex.Kamrath@live.com>
2018-10-06 17:31:03 +1000

    added dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
+class SubdomainRequired
+  def self.matches?(request)
+    request.subdomain.present? && request.subdomain != 'www'
+  end
+end
+
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'welcome#index'
+
   resource :account, only: [:new, :create]
 
+
+  constraints SubdomainRequired do
+    root to: 'dashboards#show', as: 'subdomain_root'
+  end
+
+  root to: 'welcome#index'
 end
