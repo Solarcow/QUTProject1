8b229ee8ff21b87d84c5b78062390e439a1b16d0
Solarcow <Alex.Kamrath@live.com>
2018-10-06 16:12:41 +1000

    added devise and membership model

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,12 @@
 class Account < ActiveRecord::Base
+	has_many :memberships, dependent: :destroy
+	has_many :users, through: :memberships
+
+	
 	validates :name, :subdomain, presence: true
 	validates :subdomain,
 		format: { with: /\A[a-z][a-z0-9\-]+[a-z0-9]\Z/ },
         uniqueness: true
+
+
 end
